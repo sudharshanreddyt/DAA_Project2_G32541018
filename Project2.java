197b6758b84f48f739cde94a3cce804af98ec580
tSid7710 <sudharshanreddy7710@gmail.com>
2024-09-26 01:53:50 -0400

    Added merge sequence list

--- Project2.java
+++ Project2.java
@@ -1,20 +1,10 @@
-import java.util.*;
-
-class MergeListInfo {
-    // MergeList - stores the size of the list and the index of the list in the
-    // array.
-    int size;
-    int index;
-
-    MergeListInfo(int size, int index) {
-        this.size = size;
-        this.index = index;
-    }
-}
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Project2 {
 
-    public static int calculateMergeCost(int listSizes[]) {
+    public static int mergeSequenceAndCost(int listSizes[]) {
         long startTime = System.nanoTime();
 
         if (listSizes.length < 2) {
@@ -22,66 +12,49 @@ public class Project2 {
         }
 
         int totalCost = 0;
-        int mergeIndex = 0;
-        int mergeSequence[] = new int[listSizes.length];
+        ArrayList<String> mergeSequence = new ArrayList<>();
 
         // Priority Queue to store lists based on their sizes (smallest first).
-        PriorityQueue<MergeListInfo> minHeap = new PriorityQueue<MergeListInfo>(new Comparator<MergeListInfo>() {
-            public int compare(MergeListInfo l1, MergeListInfo l2) {
-                return l1.size - l2.size;
-            }
-        });
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
 
-        // Add all input array elements (sizes) to the minHeap.
+        // Add all input array elements (list sizes) to the minHeap.
         for (int i = 0; i < listSizes.length; i++) {
-            minHeap.add(new MergeListInfo(listSizes[i], i));
+            minHeap.add(listSizes[i]);
         }
 
         int mergeCount = 1;
         while (mergeCount < listSizes.length) {
 
-            // Extract two smallest lists.
-            MergeListInfo list1 = minHeap.poll();
-            MergeListInfo list2 = minHeap.poll();
+            // Extract two smallest list sizes.
+            int list1 = minHeap.poll();
+            int list2 = minHeap.poll();
 
             // Calculate the merge cost of the two smallest lists.
-            int mergeCost = list1.size + list2.size;
+            int mergeCost = list1 + list2;
 
             // Record the merging order.
-            if (list1.index != -1) {
-                mergeSequence[mergeIndex++] = list1.index;
-            }
-            if (list2.index != -1) {
-                mergeSequence[mergeIndex++] = list2.index;
-            }
+            mergeSequence.add("Merged " + list1 + " and " + list2);
 
             // Accumulate total merge cost.
             totalCost = totalCost + mergeCost;
 
-            // Add the merged list back into the minHeap.
-            MergeListInfo mergedList = new MergeListInfo(mergeCost, -1); // -1 to indicate this is a merged node.
-            minHeap.add(mergedList);
+            // Add the merged lists cost back into the minHeap.
+            minHeap.add(mergeCost);
             mergeCount++;
         }
 
-        // Print the order of list merges.
-        // System.out.println("Merge Order: ");
-        // for (int i = 0; i < mergeIndex; i++) {
-        // System.out.print(mergeSequence[i] + " ");
-        // }
-
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
-        System.out.println("\nDuration for n = " + listSizes.length + " is : " + duration);
+        System.out.println("Duration for n = " + listSizes.length + " is : " + duration);
         return totalCost;
     }
 
     public static void main(String args[]) {
-        int testArr[] = { 10, 100, 1000, 10000, 100000, 1000000 };
+        int testArr[] = { 100, 1000, 10000, 100000, 1000000 };
         for (int size : testArr) {
             int a[] = generateRandomListWithSize(size);
-            int totalCost = calculateMergeCost(a);
-            System.out.println("\nFinal total cost: " + totalCost);
+            int totalCost = mergeSequenceAndCost(a);
+            System.out.println("Final total cost: " + totalCost);
         }
     }
 
